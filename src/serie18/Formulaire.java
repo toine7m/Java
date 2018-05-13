e26646230e0d58c5fea40821c4b8e5d2ccdf084d
Antoine Massinon <toine7m@gmail.com>
2018-05-13 22:32:30 +0200

    Update
    des broutilles dans celui-ci, suppression de variables unused mais il en reste pour sûr j'i fais ça vite fait

--- src/serie18/Formulaire.java
+++ src/serie18/Formulaire.java
@@ -25,10 +25,8 @@ import javax.swing.JTextField;
 
 public class Formulaire extends JPanel{
 
-	
-	private JTextField txtInstallation;
 	private JCheckBox typeInstallation;  // oui non ? qu'attendez vous ?
-	private JTextField commentaires, dureeInstallation, reference, dateInstallation;
+	private JTextField commentaires, dureeInstallation, reference;
 	private JRadioButton aPrevoir, termine, enCours;
 	private ButtonGroup validation;
 	private JComboBox<Object> codeSW, matricule, codeOS; 
@@ -66,21 +64,21 @@ public class Formulaire extends JPanel{
 		commentaires = new JTextField();
 		add(commentaires);
 		
-		add(new JLabel("Quel est la durée de l'installation ? (Obligatoire):"));
+		add(new JLabel("Quel est la durÃ©e de l'installation ? (Obligatoire):"));
 		dureeInstallation = new JTextField();
 		add(dureeInstallation);
 		
-		add(new JLabel("Quel est la référence de l'installation ? :"));
+		add(new JLabel("Quel est la rÃ©fÃ©rence de l'installation ? :"));
 		reference = new JTextField();
 		add(reference);
 		
 		add(new JLabel("Le statut de l'installation est :"));
 		validation = new ButtonGroup();
-		aPrevoir = new JRadioButton("A prévoir");
+		aPrevoir = new JRadioButton("A prÃ©voir");
 		validation.add(aPrevoir);
 		add(aPrevoir);
 		aPrevoir.addActionListener(ecouteur);
-		termine = new JRadioButton("Terminé");
+		termine = new JRadioButton("TerminÃ©");
 		validation.add(termine);
 		add(termine);
 		termine.addActionListener(ecouteur);
@@ -94,10 +92,10 @@ public class Formulaire extends JPanel{
 		add(dateInstallation);*/
 		
 				
-		p = new PanneauDate(); // désactiver par défaut
+		p = new PanneauDate(); 
 		add(p);
-		p.setEnabled(false);
-		initialiserTableaux(); //les jcombobox on besoin  de tableau pour afficher les contenu de la DB
+		p.setEnabled(false); // dÃ©sactiver par dÃ©faut
+		initialiserTableaux(); //les jcombobox ont besoin  de tableau pour afficher les contenu de la DB
 		
 		add(new JLabel("Code Software"));
 		codeSW = new JComboBox<Object>(tabCodeSW);
@@ -111,20 +109,13 @@ public class Formulaire extends JPanel{
 		codeOS = new JComboBox<Object>(tabCodeOS);
 		add(codeOS);
 		
-		insertion = new JButton("Insérer");
+		insertion = new JButton("InsÃ©rer");
 		add(insertion);
 		insertion.addActionListener(ecouteur);
 		
-		reset = new JButton("Réinitialiser");
+		reset = new JButton("RÃ©initialiser");
 		add(reset);
 		reset.addActionListener(ecouteur);
-		
-		
-		
-	
-		
-		
-		
 	}
 	
 	private void initialiserTableaux(){
@@ -155,9 +146,9 @@ public class Formulaire extends JPanel{
 				if(valide()) {
 					try {
 						preparation = f.getCo().prepareStatement("SELECT max(IdInstallation) FROM Installation");
-						int newID = (int) AccessBDGen.creerListe1Colonne(preparation)[0]; // on prend le premeire element du tableau qui est renvoyé (façon racourci de prendre la premiere cellule du tableau et de la metre dans NEWID
-						newID++; // incrémenter l ID sinon erreur (2x un enregistrement avec la même clé sa pète !!!
-						String dateAinserer = pdate.getText();// on récupere la date que l'utlisateur a mit
+						int newID = (int) AccessBDGen.creerListe1Colonne(preparation)[0]; // on prend le premier element du tableau qui est renvoyÃ© (faÃ§on racourci de prendre la premiere cellule du tableau et de la metre dans NEWID
+						newID++; // incrÃ©menter l ID sinon erreur (2x un enregistrement avec la mÃªme clÃ© Ã§a pÃ¨te mdr !!!
+						String dateAinserer = pdate.getText();// on rÃ©cupere la date que l'utlisateur a mit
 						preparation = f.getCo().prepareStatement("INSERT INTO Installation ("
 								+ "IdInstallation, "
 								+ "DateInstallation, "
@@ -175,19 +166,19 @@ public class Formulaire extends JPanel{
 						preparation.setDate(2, Date.valueOf(dateAinserer));
 						
 						boolean type;
-						type = typeInstallation.isSelected(); //true if selected donc case coché or false if not coché)
-						preparation.setBoolean(3, type); // on récupère le type
+						type = typeInstallation.isSelected(); //true if selected donc case cochÃ© or false if not cochÃ©)
+						preparation.setBoolean(3, type); // on rÃ©cupÃ¨re le type
 						
 						String com;
-						com = commentaires.getText(); // on va rechercher ce que l user a inserer dans commantaires
+						com = commentaires.getText(); // on va rechercher ce que l user a inserer dans commentaires
 						if(com.equals("")){
 							preparation.setString(4, null); // soit on recupere rien
 						}else{
-							preparation.setString(4, com); // soit on récupere le commantaire com
+							preparation.setString(4, com); // soit on rÃ©cupere le commentaire com
 						}
 						
 						int temps;
-						temps = Integer.parseInt(dureeInstallation.getText()); // fonction spéciale qui caste le string en int car dureeinstalle c'est un jtextfield et je lui donne en argument la séquence
+						temps = Integer.parseInt(dureeInstallation.getText()); // fonction spÃ©ciale qui caste le string en int car dureeinstalle c'est un jtextfield et je lui donne en argument la sÃ©quence
 						preparation.setInt(5, temps);
 						
 						String ref;
@@ -199,42 +190,42 @@ public class Formulaire extends JPanel{
 						}
 						
 						if(aPrevoir.isSelected()){  // test dans un buttungroup pas besoin de else puisque on ne peut selectionner qu'un seul champ
-							preparation.setString(7, "à prévoir");
+							preparation.setString(7, "Ã  prÃ©voir");
 							String dateIns;
-							dateIns = p.getText(); // on récupere la date de l'user
+							dateIns = p.getText(); // on rÃ©cupere la date de l'user
 							Date dateInsFor;
-							dateInsFor = Date.valueOf(dateIns); // on converti la date de l'user au format de la méthode que propose date.valueof
+							dateInsFor = Date.valueOf(dateIns); // on converti la date de l'user au format de la mÃ©thode que propose date.valueof
 							preparation.setDate(8, dateInsFor); // on envoie la date
 						}if(termine.isSelected()){
-							preparation.setString(7, "terminé");
-							preparation.setDate(8, null); // aucune date a prévoir pour ce champ et l'autre
+							preparation.setString(7, "terminÃ©");
+							preparation.setDate(8, null); // aucune date a prÃ©voir pour ce champ et l'autre
 						}if(enCours.isSelected()){
 							preparation.setString(7, "en cours");
 							preparation.setDate(8, null);
 						}
 						
 						String codeSWchoisi;
-						codeSWchoisi = codeSW.getSelectedItem().toString(); // on récupere l'element de l user a la coimbobox et on caste en string pour pas qu'il rale	
+						codeSWchoisi = codeSW.getSelectedItem().toString(); // on rÃ©cupere l'element de l user a la coimbobox et on caste en string pour pas qu'il rale	
 						preparation.setString(9, codeSWchoisi);
 						
 						String mat;
-						mat = matricule.getSelectedItem().toString(); // on récupere l'element de l user a la coimbobox et on caste en string pour pas qu'il rale	
+						mat = matricule.getSelectedItem().toString(); // on rÃ©cupere l'element de l user a la coimbobox et on caste en string pour pas qu'il rale	
 						preparation.setString(10, mat);
 						
 						String oscode;
-						oscode = codeOS.getSelectedItem().toString(); // on récupere l'element de l user a la coimbobox et on caste en string pour pas qu'il rale	
+						oscode = codeOS.getSelectedItem().toString(); // on rÃ©cupere l'element de l user a la coimbobox et on caste en string pour pas qu'il rale	
 						preparation.setString(11, oscode);
 						
-						preparation.executeUpdate(); // exécute les preparation
+						preparation.executeUpdate(); // exÃ©cute les preparation
 						preparation.close();
-						JOptionPane.showMessageDialog(null, "Vous venez d'encoder les informations nécessaires", "Félicitation", JOptionPane.INFORMATION_MESSAGE);
+						JOptionPane.showMessageDialog(null, "Vous venez d'encoder les informations nÃ©cessaires", "FÃ©licitation", JOptionPane.INFORMATION_MESSAGE);
 						
 					} catch (SQLException e1) {
 						// TODO Auto-generated catch block
 						e1.printStackTrace();
 					}
 				}else {
-					JOptionPane.showMessageDialog(null, "Vous n'avez pas rempli correctement les champs demandés, vérifiez les dates, et autres contenus","Erreur", JOptionPane.ERROR_MESSAGE);
+					JOptionPane.showMessageDialog(null, "Vous n'avez pas rempli correctement les champs demandÃ©s, vÃ©rifiez les dates, et autres contenus","Erreur", JOptionPane.ERROR_MESSAGE);
 					
 				}
 				
@@ -242,7 +233,7 @@ public class Formulaire extends JPanel{
 			if (e.getSource().equals(reset)){
 				for(Component control : Formulaire.this.getComponents())  // remet tout les jtext du pannel a rien et pareilp our comboBox
 				{//fonction qui prend tout les element dans le pannel courant et qui les renvoie sous la forme d un tableau et le for en racourci pour boucler sur tout les elements
-				    if(control instanceof JTextField) // control représente l'element sur lequel on est
+				    if(control instanceof JTextField) // control reprÃ©sente l'element sur lequel on est
 				    {									//https://stackoverflow.com/questions/21524117/how-to-clear-all-input-fields-within-a-jpanel
 				        JTextField ctrl = (JTextField) control;
 				        ctrl.setText("");
@@ -260,7 +251,7 @@ public class Formulaire extends JPanel{
 				}
 				
 			}
-			if (e.getSource().equals(aPrevoir)){ // si l user a cliquer sur a prévoir
+			if (e.getSource().equals(aPrevoir)){ // si l user a cliquer sur a prÃ©voir
 				p.setEnabled(true);	
 			}if (e.getSource().equals(termine)){
 				p.setEnabled(false);
